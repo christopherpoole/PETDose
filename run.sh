27150d7b7782b0e4f0750c81dbd25b595e002acc
Christopher Poole <mail@christopherpoole.net>
2013-09-10 10:00:00 +1000

    Added command line params to run

--- run.sh
+++ run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-cd /media/nfsroot/software/cern/geant4.9.6.p02/bin
+cd /media/nfsroot/software/cern/geant4.9.6/bin
 source geant4.sh
 cd ~
 
 export G4LEDATA=/media/nfsroot/software/cern/geant4.9.6/data/G4EMLOW6.32
-export PYTHONPATH=$PYTHONPATH:/media/nfsroot/software/cern/geant4.9.6.p02/python/lib
+export PYTHONPATH=$PYTHONPATH:/media/nfsroot/software/cern/geant4.9.6/python/lib
 
 # Other
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib
 
 cd /media/nfsroot/software/PETDose
 
-python -B main.py $macro $dicom_dir $pet_aq $ct_aq $histories $PBS_ARRAYID
-
+python -B main.py --run_id=$PBS_ARRAYID --radius=421 --width=3 --length=10 --crystals_phi=1 --crystals_axis=118 --heads=672 --histories=10000 --dicom=/media/nfsroot/software/PETDose/data --ct_acquisition=2 --save=True --gun_x=$gunx --gun_y=$guny --gun_z=$gunz
